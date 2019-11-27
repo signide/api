e77c3c442f02903e3b6b2b1ee32ed7331719a3f4
kakashi000 <rrk061295@gmail.com>
2019-11-27 14:10:38 +0800

    Write enough tests to cover all routes

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -3,18 +3,15 @@ import faker from "faker/locale/en";
 import { expect } from "chai";
 import { app } from "../app";
 import { query, pool } from "../db/query";
-import {
-  getUserByName,
-  deleteUser,
-  createUser,
-  getUser
-} from "../routes/users/model";
-import { getEntry } from "../routes/entries/model";
+import { deleteUser, createUser, getUser } from "../routes/users/model";
+import { getEntry, deleteEntry } from "../routes/entries/model";
 
 const jwtRegex = /^.*\..*\..*$/;
-const username = "randomUsername12345";
+const username = "TESTuser";
+const adminUsername = "TESTadmin";
 const password = faker.internet.password();
 const email = faker.internet.email();
+
 let userToken;
 let user;
 let entryID;
@@ -23,9 +20,14 @@ let admin;
 
 describe("end-to-end testing", () => {
   before(async () => {
+    const result = await query(
+      `SELECT * FROM users WHERE username IN ('${username}', '${adminUsername}')`
+    );
+    result.rows.forEach(async user => await deleteUser(user.id));
+
     const created = await createUser(
       {
-        username: "!@#admin",
+        username: adminUsername,
         password,
         email: faker.internet.email()
       },
@@ -35,13 +37,8 @@ describe("end-to-end testing", () => {
   });
 
   after(async () => {
-    const selected = await getUserByName(username);
-    if (!selected) {
-      return;
-    }
-    const userID = selected.id;
-    await query("DELETE FROM entries WHERE user_id=$1", [userID]);
-    await deleteUser(userID);
+    await deleteEntry(entryID);
+    await deleteUser(user.id);
     await deleteUser(admin.id);
     await pool.end();
   });
@@ -72,6 +69,33 @@ describe("end-to-end testing", () => {
     expect(response.status).equal(201);
     adminToken = response.body.token;
     expect(adminToken).match(jwtRegex);
+  });
+
+  it("gets a user", async () => {
+    const response = await request(app)
+      .get(`/users/${user.id}`)
+      .set("Authorization", `Bearer ${adminToken}`);
+
+    expect(response.status).equal(200);
+    expect(response.body.id).equal(user.id);
+  });
+
+  it("gets user info from the token", async () => {
+    const response = await request(app)
+      .get("/users/me")
+      .set("Authorization", `Bearer ${userToken}`);
+
+    expect(response.status).equal(200);
+    expect(response.body.id).equal(user.id);
+  });
+
+  it("gets all users", async () => {
+    const response = await request(app)
+      .get("/users")
+      .set("Authorization", `Bearer ${adminToken}`);
+
+    expect(response.status).equal(200);
+    expect(response.body[0]).to.have.property("id");
   });
 
   it("changes the user's password", async () => {
@@ -114,6 +138,24 @@ describe("end-to-end testing", () => {
 
     expect(response.status).equal(200);
     expect(response.body.id).equal(entryID);
+  });
+
+  it("gets all entries", async () => {
+    const response = await request(app)
+      .get("/entries")
+      .set("Authorization", `Bearer ${adminToken}`);
+
+    expect(response.status).equal(200);
+    expect(response.body[0]).to.have.property("id");
+  });
+
+  it("gets the user's averages", async () => {
+    const response = await request(app)
+      .get(`/users/${user.id}/average`)
+      .set("Authorization", `Bearer ${userToken}`);
+
+    expect(response.status).equal(200);
+    expect(response.body).to.have.property("distance");
   });
 
   it("deletes an entry", async () => {
