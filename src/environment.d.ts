281091bbee975ab44bf00aece611904e6bec65ce
kakashi000 <rrk061295@gmail.com>
2019-12-14 19:00:22 +0800

    Change all env typings to string

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,18 +1,18 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    API_PORT: number;
-    API_IN_PRODUCTION: boolean;
+    API_PORT: string;
+    API_IN_PRODUCTION: string;
     JWT_SECRET: string;
-    JWT_EXPIRE_TIME: string | number;
+    JWT_EXPIRE_TIME: string;
     KEYS_WEATHER: string;
     TYPEORM_CONNECTION: string;
     TYPEORM_HOST: string;
     TYPEORM_USERNAME: string;
     TYPEORM_PASSWORD: string;
     TYPEORM_DATABASE: string;
-    TYPEORM_PORT: number;
-    TYPEORM_SYNCHRONIZE: boolean;
-    TYPEORM_LOGGING: boolean;
+    TYPEORM_PORT: string;
+    TYPEORM_SYNCHRONIZE: string;
+    TYPEORM_LOGGING: string;
     TYPEORM_ENTITIES: string;
     TEST_DATABASE: string;
   }
